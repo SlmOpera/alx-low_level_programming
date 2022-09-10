6e02d4e98dbf87d23f644751d1a893a2636cf94b
Ifeoluwa Akinniyi <akinniyiifeoluwa1@gmail.com>
2022-09-10 10:06:03 -0700

    puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
   * main - Entry point
   *
-  * description: C program that prints programming is like a multilingual puzzle
+  * Description: print programming is like a multilingual puzzle
   *
-  * return: Always 0 (Success)
+  * Return: Always 0 (Success)
   */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
