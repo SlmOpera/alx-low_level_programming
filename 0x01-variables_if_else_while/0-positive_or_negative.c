04d5f9e72b031ed5c58a63fe95832449bf30cca5
Ifeoluwa Akinniyi <akinniyiifeoluwa1@gmail.com>
2022-09-11 14:54:39 -0700

    betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /*
- * 
+ *
  * main - Entry point
  *
- * Description: program will assign a random number to the variable n each time it is executed
+ * Description: program will assign a random number to the variable n
  *
  * Return: Always 0 (Success)
  */
