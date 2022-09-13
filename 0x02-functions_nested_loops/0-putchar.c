3e2cd9cefa72e0c9369d93e82a180c5f23c2e818
Ifeoluwa Akinniyi <akinniyiifeoluwa1@gmail.com>
2022-09-13 15:49:29 -0700

    lets go

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _puthchar, followed by a new line
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
