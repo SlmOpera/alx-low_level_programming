eb8a7cc729c296957e1e003a7dc4f76b4c3f3df5
Ifeoluwa Akinniyi <akinniyiifeoluwa1@gmail.com>
2022-09-14 12:21:17 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main  - this program prints putchar
+ *
+ * Return - Always (Success)
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('e');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
